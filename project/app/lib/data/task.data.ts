eb4ebddaa3aad13d179615f1f458708d8454b7b8
Miriam Kisos <87091443+MKisos30@users.noreply.github.com>
2024-11-15 14:14:37 +0200

    new task

--- project/app/lib/data/task.data.ts
+++ project/app/lib/data/task.data.ts
@@ -1,32 +1,70 @@
-import connectToDatabase from "../mongodb";
-import Task from "../taskModel";
+import connectToDatabase from '../mongodb';
+import Task from '../taskModel';
+import { z } from 'zod';
+import { revalidatePath } from '@/node_modules/next/cache';
+import { redirect } from '@/node_modules/next/navigation';
 
 export const getAllTasks = async () => {
-    try {
-        await connectToDatabase()
-        const allTasks = await Task.find({})
+  try {
+    await connectToDatabase();
+    const allTasks = await Task.find({});
 
-        return allTasks;
-    } catch (error) {
-        console.log(error)
-    }
-}
+    return allTasks;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export type State = { //continue the state + validation
-    errors?: {
+export type State = {
+  //continue the state + validation
+  errors?: {
+    title?: string[];
+    done?: string[];
+    description?: string[];
+    ddLine?: string[];
+    adress?: string[];
+  };
+  message?: string | null;
+};
 
-    }
-    message?: string | null;
-}
+const taskValidation = z.object({
+  title: z
+    .string({ invalid_type_error: 'חייב לכלול אותיות' })
+    .min(10, { message: 'מינימום 10 אותיות' }),
+  description: z
+    .string({ invalid_type_error: 'חייב לכלול אותיות' })
+    .min(5, { message: 'מינימום 5 אותיות' }),
+  ddLine: z.date().min(10, { message: 'מינימום 10 תווים' }),
+  adress: z.string({ invalid_type_error: 'חייב להכיל אותיות בלבד' }),
+});
 
 export const newTask = async (prevState: State, formData: FormData) => {
-    try {
-        
+  try {
+    const taskValid = taskValidation.safeParse({
+      title: formData.get('title'),
+      description: formData.get('description'),
+      ddLine: formData.get('ddLine'),
+      adress: formData.get('adress'),
+    });
 
-    } catch (error) {
-        console.log(error)
+    if (!taskValid.success) {
+      return {
+        errors: taskValid.error.flatten().fieldErrors,
+        message: 'שגיאה, המשימה לא נוצרה',
+      };
     }
-}
 
+    const { title, description, ddLine, adress } = taskValid.data;
 
+    await connectToDatabase();
+    await Task.create({title, description, ddLine, adress})
 
+    revalidatePath('/home')    // update cache of the page that you want to update 
+    redirect('home')   //redirect to the page that you want
+    // const newTask = await new Task({ title, description, ddLine, adress })
+    // await newTask.save()
+
+  } catch (error) {
+    console.log(error);
+  }
+};
